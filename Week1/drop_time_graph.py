f065213199b2beddfc4ba3796e9d50649ac781d6
SF-24 <legonexoknights123@gmail.com>
2026-01-09 21:35:00 +0000

    Updated distance/time graph sketch

--- Week1/drop_time_graph.py
+++ Week1/drop_time_graph.py
@@ -2,6 +2,9 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Matplotlib docs: https://matplotlib.org
+# Colour list: https://matplotlib.org/stable/gallery/color/named_colors.html
+
 g = 9.81
 xMax = 30.0
 fontSize=12
@@ -16,10 +19,11 @@ for i in x:
 
 # Plot a graph of plot time against drop distance.
 
-plt.plot(x,t,'r-','linewidth',2)
+plt.plot(x,t,'r-',linewidth=2)
 plt.box(on=True)
 plt.setp(plt.xlabel('x'),fontsize=fontSize)
 plt.setp(plt.ylabel('y'),fontsize=fontSize)
+plt.grid(color='silver', linestyle='-', linewidth=1, axis='both')
 plt.xlabel('distance: x/m')
 plt.ylabel('time: t/s')
 plt.title('Distance dropped vs time taken without air resistance')
