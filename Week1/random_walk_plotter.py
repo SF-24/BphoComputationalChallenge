8d314c5a1f5c61dad8f527fea0b690b4ade0f8cc
SF-24 <legonexoknights123@gmail.com>
2026-01-15 21:54:07 +0000

    Added random walk generator

--- Week1/random_walk_plotter.py
+++ Week1/random_walk_plotter.py
@@ -1,1 +1,40 @@
 # TODO: Do later
+# Take a fixed step of l metres
+# Between each step change the direction by a random value
+# Plot first "n" steps.
+import math
+# Imports and disables interactive display
+import matplotlib; matplotlib.use('Agg'); import matplotlib.pyplot as plt
+import numpy as np
+
+# Optimises line rendering at some cost of quality
+plt.rcParams['path.simplify'] = True
+
+# Func. generates random motion from origin
+def random_walk_demo(step_length, steps):
+    x=[0.0]
+    y=[0.0]
+    for i in range(int(n)):
+        theta=2*math.pi*np.random.random()
+        dx = l*math.cos(theta)
+        dy = l*math.sin(theta)
+        x.append(x[i]+dx)
+        y.append(y[i]+dy)
+    return [x,y]
+
+n = 1e6 # Step count (-1)
+l = 1   # Step size
+fig, ax = plt.subplots()
+# Pregenerate colours
+RGB=np.random.random((20,3))
+
+# Iterate
+for k in range(20):
+    [x_,y_]=random_walk_demo(l,n)
+    # plot the random walk
+    ax.plot(x_,y_, color=RGB[k])
+
+# Show result
+print("Showing axis")
+plt.show()
+print("Code execution finished")
