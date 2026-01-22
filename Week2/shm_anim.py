e0d335f0ed3d48bc4286b4f04c5c7851a68ccf8e
SF-24 <legonexoknights123@gmail.com>
2026-01-22 21:35:37 +0000

    Updated harmonic motion with damping

--- Week2/shm_anim.py
+++ Week2/shm_anim.py
@@ -2,7 +2,6 @@
 # a = - (omega)^2 * x
 #
 import math
-from configparser import MAX_INTERPOLATION_DEPTH
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -11,22 +10,27 @@ import matplotlib ; matplotlib.use('TkAgg')  # or 'nbAgg' for Jupyter
 radius = 0.1
 x = [-1.0]
 v = [0.0]
+t = [0.0]
 T = 1.0 # period in seconds
 omega = 2*math.pi/T
 
+k=1.0 # constant for resistive force.
+# SET TO k=0 FOR SIMPLE HARMONIC MOTION
+# SET TO k=10 FOR CRITICAL DAMPING
+
 dt = 0.01 # time change in seconds.
 
-maxIterations = 10000
+maxIterations = 1000
 
 # Generate dataset
 for n in range(maxIterations):
-    a = -math.pow(omega,2)*x[n]
+    a = -math.pow(omega,2)*x[n] -k*v[n]
     v.append(v[n] + a*dt)
     x.append(x[n] + v[n+1]*dt)
-    print("Iteration: " + str(n) + " Position: " + str(x[n]))
+    t.append(t[n]+dt)
 
-fig, ax = plt.subplots()
-ax.set_aspect("equal")
+fig, ax = plt.subplots(2)
+ax[0].set_aspect("equal")
 # Define the ball object
 
 def init():
@@ -43,16 +47,14 @@ def animate(i):
     return [ball]
 
 def get_x_pos_from_iteration(iteration):
-    print("iteration: " + str(iteration))
     if iteration>=maxIterations:
-        print("Max iterations exceeded")
         return x[int(maxIterations-1)]
     return x[int(iteration)]
 
 ball = plt.Circle((x[0],0),radius)
-ax.add_patch(ball)
-ax.set_xlim(-1.2,1.2)
-ax.set_ylim(-0.5,0.5)
+ax[0].add_patch(ball)
+ax[0].set_xlim(-1.2,1.2)
+ax[0].set_ylim(-0.5,0.5)
 
 ani = animation.FuncAnimation(
     fig,
@@ -64,6 +66,10 @@ ani = animation.FuncAnimation(
     cache_frame_data=True,
     init_func=init
 )
+ax[1].set_xlim(0.0,maxIterations*dt)
+ax[1].set_ylim(-1.0,1.0)
+ax[1].plot(t,x)
 plt.ion()
 plt.show(block=True)
+plt.pause(2.0)
 
