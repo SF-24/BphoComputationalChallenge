c6bf72fcbf3ba68fef97ca3f3d37ca94c64fe51e
SF-24 <legonexoknights123@gmail.com>
2026-05-03 22:21:07 +0100

    c

--- Week3/advanced_gravitational_motion_example.py
+++ Week3/advanced_gravitational_motion_example.py
@@ -5,9 +5,13 @@ solar_mass=1988475000000000000000000000000
 # Create the objects. You can add as many as you want,
 # However, the more objects you have, the more resources it will require to run.
 asi.create_object(solar_mass, 0, 0, 500, 0, 0.2, "red")
-asi.create_object(solar_mass/10, 2.0*AU, 1.0*AU, -10000, -1000, 0.1, "blue")
+# asi.create_object(solar_mass/10, 2.0*AU, 1.0*AU, -10000, -1000, 0.1, "blue")
 asi.create_object(solar_mass/10, -2.0*AU, 1.0*AU, 10000, 100, 0.1, "green")
 asi.create_object(solar_mass/100, 0, -1.0*AU, 120, 1000, 0.1, "orange")
+asi.create_object(solar_mass/100, 1.0, -0.25*AU, 120, 1000, 0.1, "blue")
+
+# Extra mass
+asi.create_object(solar_mass/10000, 0, 1.0*AU, -12000, 1000, 0.1, "purple")
 
 # Simulate the motion and render it
 asi.simulate()
